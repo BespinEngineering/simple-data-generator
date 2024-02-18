74c80a94ec254447c1f9184e4b51e919b8564521
AJ Pahl <ajpahl1008@gmail.com>
2024-02-17 23:12:39 -0500

    Skip TLS if config is only http

--- build_docker_keystore.bash
+++ build_docker_keystore.bash
@@ -1,15 +1,19 @@
 #!/bin/bash
 PASSWORD="`cat /config/sdg.yml | grep keystorePassword | awk -F\: '{print $2}'| tr -d '[:blank:]'`"
 
-HOST="`cat /config/sdg.yml | grep elasticsearchHost | awk -F\: '{printf $2}' | tr -d '[:blank:]'`"
-PORT="`cat /config/sdg.yml | grep elasticsearchPort | awk -F\: '{printf $2}' | tr -d '[:blank:]'`"
+SCHEME="`cat /config/sdg.yml | grep backendHost | awk -F\: '{printf $2}' | tr -d '[:blank:]'`"
 
-openssl s_client -connect ${HOST}:${PORT} </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /app/es_cloud.cert
+if ["$SCHEME" == "https"]; then
+   HOST="`cat /config/sdg.yml | grep backendHost | awk -F\: '{printf $2}' | tr -d '[:blank:]'`"
+   PORT="`cat /config/sdg.yml | grep backendPort | awk -F\: '{printf $2}' | tr -d '[:blank:]'`"
 
-keytool -genkey -alias elastic_servers \
-    -keyalg RSA -keystore /app/keystore.jks \
-    -dname "CN=DockerPerson, OU=DockerImageMakers, O=Persons L=Atlanta, S=Georgia, C=US" \
-    -storepass ${PASSWORD} -keypass ${PASSWORD}
+   openssl s_client -connect ${HOST}:${PORT} </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /app/es_cloud.cert
 
-keytool -import -noprompt -trustcacerts -alias elastic_cloud -file es_cloud.cert \
-    -keystore ./keystore.jks -storepass ${PASSWORD}
+   keytool -genkey -alias elastic_servers \
+       -keyalg RSA -keystore /app/keystore.jks \
+       -dname "CN=DockerPerson, OU=DockerImageMakers, O=Persons L=Atlanta, S=Georgia, C=US" \
+       -storepass ${PASSWORD} -keypass ${PASSWORD}
+
+   keytool -import -noprompt -trustcacerts -alias elastic_cloud -file es_cloud.cert \
+       -keystore ./keystore.jks -storepass ${PASSWORD}
+fi
