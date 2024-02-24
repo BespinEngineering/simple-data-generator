fff4eec2a6b768844a2873b972cbf58786ee1238
AJ Pahl <ajpahl1008@gmail.com>
2024-02-24 10:29:46 -0500

    Refactored keystore script logic and stdout

--- build_docker_keystore.bash
+++ build_docker_keystore.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCHEME="`cat /config/sdg.yml | grep backendScheme | awk -F\: '{printf $2}' | tr -d '[:blank:]'`"
+BACKEND_TYPE="`cat /config/sdg.yml | grep backendType | awk -F\: '{printf $2}' | tr -d '[:blank:]'`"
 
 if [ "$SCHEME" == "https" ]; then
 
@@ -8,13 +9,20 @@ if [ "$SCHEME" == "https" ]; then
    HOST="`cat /config/sdg.yml | grep backendHost | awk -F\: '{printf $2}' | tr -d '[:blank:]'`"
    PORT="`cat /config/sdg.yml | grep backendPort | awk -F\: '{printf $2}' | tr -d '[:blank:]'`"
 
-   openssl s_client -connect ${HOST}:${PORT} </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /app/es_cloud.cert
+   if [ "$BACKEND_TYPE" == "ELASTICSEARCH" ]; then
+      echo "Creating Keystore entry for Elasticsearch Secure HTTP endpoint"
 
-   keytool -genkey -alias elastic_servers \
-       -keyalg RSA -keystore /app/keystore.jks \
-       -dname "CN=DockerPerson, OU=DockerImageMakers, O=Persons L=Atlanta, S=Georgia, C=US" \
-       -storepass ${PASSWORD} -keypass ${PASSWORD}
+      openssl s_client -connect ${HOST}:${PORT} </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /app/es_cloud.cert
 
-   keytool -import -noprompt -trustcacerts -alias elastic_cloud -file es_cloud.cert \
-       -keystore ./keystore.jks -storepass ${PASSWORD}
+      keytool -genkey -alias elastic_servers \
+        -keyalg RSA -keystore /app/keystore.jks \
+        -dname "CN=DockerPerson, OU=DockerImageMakers, O=Persons L=Atlanta, S=Georgia, C=US" \
+        -storepass ${PASSWORD} -keypass ${PASSWORD}
+
+      keytool -import -noprompt -trustcacerts -alias elastic_cloud -file es_cloud.cert \
+          -keystore ./keystore.jks -storepass ${PASSWORD}
+  fi
+
+else
+  echo "NO Secure Endpoint defined - only HTTP, bypassing Keystore creation"
 fi
